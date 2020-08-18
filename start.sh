f0a031f50fd2f777dc16bb174ee99f98326c7894
yzb0212 <40413265+yzb0212@users.noreply.github.com>
2020-08-18 10:53:16 +0800

    Add files via upload
    Xavier_start

---
+++ start.sh
@@ -0,0 +1,12 @@
+sudo nvpmodel --query
+echo ------------------------------------
+sudo jetson_clocks --show
+echo ------------------------------------
+sudo jetson_clocks
+
+sudo nvpmodel -m 0
+
+sudo nvpmodel --query
+echo ------------------------------------
+sudo jetson_clocks --show
+echo ------------------------------------
